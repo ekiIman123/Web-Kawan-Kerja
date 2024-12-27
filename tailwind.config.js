d9325b903b3ba0a66dde74828290c477bb117857
Eki Iman <ekiimanjordiansyah212@gmail.com>
2024-12-27 20:15:52 +0700

    build admin layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,44 +20,55 @@ module.exports = {
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
       },
+      fontSize: {
+        title1: '48px',
+        title2: '34px',
+        title3: '24px',
+        title4: '24px',
+        body1: '16px',
+        body2: '14px',
+        caption: '12px',
+        overline: '10px',
+        overline: '10px',
+      },
     },
   },
   plugins: [
     function ({ addUtilities }) {
       addUtilities({
-        '.th1': {
-          fontSize: '48px',
-        },
-        '.th2': {
-          fontSize: '34px',
-        },
-        '.th3': {
-          fontSize: '24px',
-        },
-        '.th4': {
-          fontSize: '20px',
-        },
-        '.fontBody1': {
-          fontSize: '16px',
-        },
-        '.fontBody2': {
-          fontSize: '14px',
-        },
-        '.fontCaption': {
-          fontSize: '12px',
-        },
-        '.fontOverline': {
-          fontSize: '10px',
-        },
-        '.fontBtnSm': {
+        // '.th1': {
+        //   fontSize: '48px',
+        // },
+        // '.th2': {
+        //   fontSize: '34px',
+        // },
+        // '.th3': {
+        //   fontSize: '24px',
+        // },
+        // '.th4': {
+        //   fontSize: '20px',
+        // },
+        // '.fontBody1': {
+        //   fontSize: '16px',
+        // },
+        // '.fontBody2': {
+        //   fontSize: '14px',
+        // },
+        // '.fontCaption': {
+        //   fontSize: '12px',
+        // },
+        // '.fontOverline': {
+        //   fontSize: '10px',
+        // },
+        '.btnSm': {
           fontWeight: 600,
           fontSize: '14px',
         },
-        '.fontBtnMd': {
+        '.btnMd': {
           fontWeight: 600,
           fontSize: '16px',
         },
-        '.fontBtnLg': {
+        '.btnLg': {
           fontWeight: 600,
           fontSize: '18px',
         },
