43af154d2090e640f1c0ae8fcde73dfabb3f3f02
Eki Iman <ekiimanjordiansyah212@gmail.com>
2024-12-30 17:57:55 +0700

    finishing new post layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,34 +3,34 @@ module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {
-      colors: {
-        primary: {
-          black: '#131127',
-          white: '#FCFCFD',
-          dark: '#FBAB18',
-        },
-        secondary: {
-          blue: '#3BBBEC',
-          gray: '#B3B2B7',
-          green: '#2BC058',
-          red: '#EF453C',
-          bluegray: '#64748B',
-        },
-      },
-      fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
-      },
-      fontSize: {
-        title1: '48px',
-        title2: '34px',
-        title3: '24px',
-        title4: '24px',
-        body1: '16px',
-        body2: '14px',
-        caption: '12px',
-        overline: '10px',
-        overline: '10px',
-      },
+      // colors: {
+      //   primary: {
+      //     black: '#131127',
+      //     white: '#FCFCFD',
+      //     dark: '#FBAB18',
+      //   },
+      //   secondary: {
+      //     blue: '#3BBBEC',
+      //     gray: '#B3B2B7',
+      //     green: '#2BC058',
+      //     red: '#EF453C',
+      //     bluegray: '#64748B',
+      //   },
+      // },
+      // fontFamily: {
+      //   poppins: ['Poppins', 'sans-serif'],
+      // },
+      // fontSize: {
+      //   title1: '48px',
+      //   title2: '34px',
+      //   title3: '24px',
+      //   title4: '24px',
+      //   body1: '16px',
+      //   body2: '14px',
+      //   caption: '12px',
+      //   overline: '10px',
+      //   overline: '10px',
+      // },
     },
   },
   plugins: [
@@ -60,26 +60,26 @@ module.exports = {
         // '.fontOverline': {
         //   fontSize: '10px',
         // },
-        '.btnSm': {
-          fontWeight: 600,
-          fontSize: '14px',
-        },
-        '.btnMd': {
-          fontWeight: 600,
-          fontSize: '16px',
-        },
-        '.btnLg': {
-          fontWeight: 600,
-          fontSize: '18px',
-        },
-        '.fontVerySmall': {
-          fontWeight: 600,
-          fontSize: '12px',
-        },
-        '.fontSmallest': {
-          fontWeight: 600,
-          fontSize: '10px',
-        },
+        // '.btnSm': {
+        //   fontWeight: 600,
+        //   fontSize: '14px',
+        // },
+        // '.btnMd': {
+        //   fontWeight: 600,
+        //   fontSize: '16px',
+        // },
+        // '.btnLg': {
+        //   fontWeight: 600,
+        //   fontSize: '18px',
+        // },
+        // '.fontVerySmall': {
+        //   fontWeight: 600,
+        //   fontSize: '12px',
+        // },
+        // '.fontSmallest': {
+        //   fontWeight: 600,
+        //   fontSize: '10px',
+        // },
       });
     },
   ],
